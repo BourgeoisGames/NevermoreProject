b9eec2b487e1d7a841ab1a7ce7e91eca5f065fd1
bonattt <bonattt@rose-hulman.edu>
2017-10-17 18:54:14 -0400

    imported unity project

---
+++ Assets/scripts/FlashlightController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour {
+
+    public bool flashlightEnabled = true;
+    public KeyCode toggleKey;
+
+    private Light flashlight;
+
+	// Use this for initialization
+	void Start () {
+        flashlight = GetComponent<Light>();
+        flashlight.enabled = flashlightEnabled;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            flashlightEnabled = !flashlightEnabled;
+            flashlight.enabled = flashlightEnabled;
+        }
+	}
+}
